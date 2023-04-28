f5bb84bfa176225d1f6b04e26edb4013ddf01cc3
rail_hail <102628186+railohail@users.noreply.github.com>
2023-04-28 20:31:54 +0800

    Add files via upload

---
+++ RPSclient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+class RPSClient
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string serverAddress = "127.0.0.1";
+            int serverPort = 8080;
+
+            TcpClient client = new TcpClient(serverAddress, serverPort);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+            Thread readThread = new Thread(() => ReadMessages(reader));
+            readThread.Start();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                writer.WriteLine(input);
+
+                if (input.Equals("quit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+            }
+
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    private static void ReadMessages(StreamReader reader)
+    {
+        try
+        {
+            while (true)
+            {
+                string message = reader.ReadLine();
+                if (message == null)
+                {
+                    break;
+                }
+
+                Console.WriteLine(message);
+            }
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Disconnected from the server.");
+        }
+    }
+}
